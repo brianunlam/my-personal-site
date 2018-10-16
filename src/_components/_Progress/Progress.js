ee05772bdb664081524b1424a518d3d5e1e8fa62
brian86 <brian.alonso.a@gmail.com>
2018-10-16 15:09:05 +0000

    Add animation to Progress Bar

--- src/_components/_Progress/Progress.js
+++ src/_components/_Progress/Progress.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
-
-
-
-
 class Progress extends Component {
+
+  constructor(props) {
+        super(props)
+        this.state = {
+            percentage: 0
+        }
+        let myInterval = setInterval(() => {
+            if(this.state.percentage < props.percentage)
+              this.setState({percentage: this.state.percentage + 1})
+            else
+              clearInterval(myInterval)
+        }, 1);
+        
+    }
   
   colorPercentage = ( percentage )=>{
-    
     if(percentage < 10){
       return "is-danger"
     }else if( percentage < 20){
@@ -19,17 +28,12 @@ class Progress extends Component {
     }
     return "is-success"
   }
-  
-  
-  
-  
-  render() {
-    const { percentage } = this.props
-    
-    return (
-  
-    <progress className={"progress is-small " + this.colorPercentage(percentage)} value={percentage} max="100">{percentage}%</progress>
 
+
+  render() {
+    const { percentage } = this.state
+    return (
+      <progress className={"progress is-small " + this.colorPercentage(this.props.percentage)} value={percentage} max="100">{percentage}%</progress>
     );
   }
 }
